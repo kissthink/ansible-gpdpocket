80f442ff1ec3befbb81dea77da204c313b9b6fed
Christopher A. Williamson <home@chrisaw.com>
2017-06-29 13:16:45 +0100

    added fix for bluetooth setup playbook

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 # install all dependencies for ansible scripts to run
 dpkg -i bootstrap/*.deb
 
-# run ansible scripts
+# run ansible scripts (without cowsay! :P)
 ANSIBLE_NOCOWS=1 ansible-playbook main.yml
