c4167b4455a13ab019dc5e641fcbe496b659d0ac
Christopher A. Williamson <home@chrisaw.com>
2017-07-03 22:02:37 +0100

    added update script and cleaned up bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# setup wifi (if necessary)
+# setup wifi
 echo "setting up wifi..."
 if [ ! -f /lib/firmware/brcm/brcmfmac4356-pcie.txt ]; then
   cp roles/wifi/files/brcmfmac4356-pcie.txt /lib/firmware/brcm/brcmfmac4356-pcie.txt
@@ -18,19 +18,12 @@ while ! ping -c1 google.com &>/dev/null; do
   sleep 1
 done
 
-# wait for apt to be available
-echo "waiting for apt to be available..."
-while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
-  sleep 1
-done
-
-# install ansible (if necessary)
-echo "ensuring ansible is installed..."
-if [ ! -f /usr/bin/ansible ]; then
-  add-apt-repository -y ppa:ansible/ansible
-  apt-get update
-  apt-get -y install ansible
-fi
+# install ansible
+echo "installing essential packages..."
+rm -f /var/lib/dpkg/lock
+add-apt-repository -y ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible unzip wget
 
 # update ansible code
 echo "downloading latest ansible code..."
@@ -39,7 +32,7 @@ wget -O /tmp/ansible-gpdpocket/master.zip https://bitbucket.org/chrisaw/ansible-
 unzip /tmp/ansible-gpdpocket/master.zip -d /tmp/ansible-gpdpocket/
 cd /tmp/ansible-gpdpocket/chrisaw-ansible*
 
-# run ansible scripts (without cowsay! :P)
+# run ansible scripts
 echo "starting ansible playbook..."
 ANSIBLE_NOCOWS=1 ansible-playbook site.yml
 
---
+++ roles/update/files/ansible-update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# update ansible code
+echo "downloading latest ansible code..."
+mkdir /tmp/ansible-gpdpocket
+wget -O /tmp/ansible-gpdpocket/master.zip https://bitbucket.org/chrisaw/ansible-gpdpocket/get/master.zip
+unzip /tmp/ansible-gpdpocket/master.zip -d /tmp/ansible-gpdpocket/
+cd /tmp/ansible-gpdpocket/chrisaw-ansible*
+
+# run ansible scripts
+echo "starting ansible playbook..."
+ANSIBLE_NOCOWS=1 ansible-playbook site.yml
+
+# cleanup
+echo "performing clean up..."
+cd
+rm -rf /tmp/ansible-gpdpocket
