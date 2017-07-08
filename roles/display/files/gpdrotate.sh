322100736806bca77d04f199fa2a3518dbe82563
Christopher A. Williamson <home@chrisaw.com>
2017-07-08 19:52:20 +0100

    big improvements to the way things work:
    - monitors.xml is no longer required
    - gpdrotate service and xsession.d scripts combine to rotate your
    display correctly automatically (for all desktop environments and login
    managers!)
    - fan handlers re-created
    - xorg config improvements (easy to switch)
    - i915.semaphores=1 added to boot options to avoid gfx-related crashes
    - reboot completed automatically ONLY for bootstrapping

---
+++ roles/display/files/gpdrotate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sleep 5
+
+DISPLAY=:0
+XAUTHORITY=$(ps aux |grep -e Xorg | head -n1 | awk '{ split($0, a, "-auth "); split(a[2], b, " "); print b[1] }')
+export DISPLAY XAUTHORITY
+
+xrandr --output DSI1 --rotate right
